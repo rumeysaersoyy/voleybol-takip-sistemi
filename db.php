d59c99443c529750ab43791777b4c7203aeaabca
Rumeysa Ersoy <22360859070@ogrenci.btu.edu.tr>
2025-06-14 15:48:11 +0300

    Son guncellemeler ve var_dump kaldirildi

--- db.php
+++ db.php
@@ -2,7 +2,7 @@
 // Veritabanı bağlantı ayarları
 $servername = "localhost"; // Genellikle hosting ortamlarında veritabanı sunucusu localhost olur
 $username = "dbusr22360859070";
-$password = "Ea5qyCFf20As"; // Şifreyi tekrar girdik, bir hata olmaması için
+$password = "Ea5qyCfF20As"; // Şifreyi tekrar girdik, bir hata olmaması için
 $dbname = "dbstorage22360859070";
 
 // Bağlantı oluşturma
